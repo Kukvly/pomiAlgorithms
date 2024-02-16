1fb620849f32d197edb41be9dc21aad50148f2f1
Kukvly <kukvly96@gmail.com>
2024-02-16 23:59:21 +0900

    B_11726 init and window git commit test

---
+++ wd_kuks_local_agrt/src/wd_kuks_local_agrt/B_11726.java
@@ -0,0 +1,23 @@
+package wd_kuks_local_agrt;
+
+/**
+ * 2 x n 크기의 직사각형을 1x2, 2x1 타일로 채우는 방법의 수를 구하기
+ * 
+ * 입력
+ * 첫째 줄에 n이 주어진다. (1<= n <= 1,000)
+ * 
+ * 출력
+ * 첫째 줄에 2xn 크기의 직사각형을 채우는 방법의 수를 10,007로 나눈 나머지 출력
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_11726 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+
+}
