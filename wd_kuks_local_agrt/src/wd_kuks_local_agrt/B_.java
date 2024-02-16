cc1e8707a1c6f5ba26d504612022fa0f33827a9b
Kukvly <kukvly96@gmail.com>
2024-02-16 23:52:49 +0900

    first commit

---
+++ wd_kuks_local_agrt/src/wd_kuks_local_agrt/B_.java
@@ -0,0 +1,13 @@
+package wd_kuks_local_agrt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_ {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+
+}
